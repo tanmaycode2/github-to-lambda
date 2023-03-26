1dbff6e15e0f84a0e7cb82da44645579c62508a6
Tanmay Shukla <96629547+tanmaycode2@users.noreply.github.com>
2023-03-26 14:59:36 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def lambda_handler(event, context):
-    d = {'col1': [1, 2], 'col2': [3, 4]}
+    d = {'col1': [1, 4], 'col2': [3, 4]}
     df = pd.DataFrame(data=d)
     print(df)
-    print("Hello, its a test 1.1X")
+    print("Hello, its a final test")
